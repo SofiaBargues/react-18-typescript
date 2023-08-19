2b40aaaab979a05782b2b1327754c8b9addc2833
Sofia Bargues <barguessofia@gmail.com>
2023-08-19 14:03:38 +0100

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { Children } from "react";
 
 function App() {
   const items = ["Rosario", "Carcarañá", "Chañar Ladeado", "San Geronimo Sur"];
 
   return (
     <div>
-      <Button onClick={() => console.log("clicked")}>Soy un boton</Button>
-      <Button variant="secondary" onClick={() => console.log("clicked")}>
-        Soy un boton
-      </Button>
+      <Alert>{"My Alert"}</Alert>
+      <Button onClick={() => console.log("clicked")}>My Button</Button>
     </div>
   );
 }
